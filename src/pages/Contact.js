a616db9450e16d9f1158801f2a65d28c6b1e8bb1
Colin Edwards <ColinJhEdwards@gmail.com>
2021-06-14 07:50:04 -0700

    fixed resume reference after renaming it

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,6 +1,6 @@
 import React from "react";
 import colin from "../Images/Colin.JPG";
-import resume from "../files/Edwards_Resume.docx.pdf";
+import resume from "../files/Edwards_Resume.pdf";
 import styled from "styled-components";
 import { motion } from "framer-motion";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
