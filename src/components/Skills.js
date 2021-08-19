b41675593db35fedb2ffe569fbf467945521d3eb
Colin Edwards <ColinJhEdwards@gmail.com>
2021-08-18 19:52:48 -0700

    changed font family for skills component to match rest of portfolio font

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -51,7 +51,7 @@ const StyledDiv = styled.div`
   height: 80%;
   display: flex;
   justify-content: space-around;
-  font-family: "Archivo Black", sans-serif;
+  /* font-family: "Archivo Black", sans-serif; */
   .lang,
   .tech,
   .study {
