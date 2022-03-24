1524f909cfa36060c4fb04e0103c83ad7cf612ca
Colin Edwards <ColinJhEdwards@gmail.com>
2022-03-23 19:21:21 -0700

    added photographer portfolio project to project page

--- src/util/data.js
+++ src/util/data.js
@@ -23,17 +23,19 @@ import socksMobileMarket from "../Images/projectImgs/socksMobileMarket.png";
 //Todo for me images
 import todoHome from "../Images/projectImgs/todoHome.png";
 import todoMobile from "../Images/projectImgs/todoMobile.png";
-//Front end Demos
-import mini3D from "../Images/projectImgs/mini3D.png";
-import miniSVG from "../Images/projectImgs/miniSVG.png";
-import miniSlide from "../Images/projectImgs/miniSlide.png";
-import miniInput from "../Images/projectImgs/miniInput.png";
+//photographer portfoilo
+import photoHome from "../Images/projectImgs/photographer/home.png";
+import photoAbout from "../Images/projectImgs/photographer/about.png";
+import photoGallery from "../Images/projectImgs/photographer/gallery.png";
+import photoContact from "../Images/projectImgs/photographer/contact.png";
+import photoMobHome from "../Images/projectImgs/photographer/mobileHome.png";
+import photoMobAbout from "../Images/projectImgs/photographer/mobileAbout.png";
+import photoMobGallery from "../Images/projectImgs/photographer/mobileGallery.png";
+import photoMobContact from "../Images/projectImgs/photographer/mobileContact.png";
 //Aperture
 import aperHome from "../Images/projectImgs/aperHome.png";
 import aperSearch from "../Images/projectImgs/aperSearch.png";
 import aperMobile from "../Images/projectImgs/aperMobile.png";
-//Weather for me
-import weather from "../Images/projectImgs/weatherHome.png";
 //Murphy's images
 import home from "../Images/projectImgs/Murphy/home.png";
 import home1 from "../Images/projectImgs/Murphy/homeDetail1.png";
@@ -61,10 +63,10 @@ export const ProjectData = () => {
       id: 1,
       title: "Inca Wheels",
       Description:
-        "Inca Wheels is an indigenous owned outdoor receational vehicle rental company based in the Pacific Northwest. Their mission is to bring affordable access to the wonderous outdoors the Pacific Northwest has to offer for generations to come.",
+        "Inca Wheels is an indigenous owned outdoor recreational vehicle rental company based in the Pacific Northwest. Their mission is to bring affordable access to the wonderous outdoors the Pacific Northwest has to offer for generations to come.",
       longDesc:
-        "Inca Wheels is an indigenous owned outdoor receational vehicle rental company based in the Pacific Northwest. Started as a way to help raise funds for the nonprofit Ruta Verde that focuses on Sustainable Development projects including the building of Community Forests in North America and creating sustainable food solutions in Peru. The Inca were honorable people that valued their surrounding environments at the core of their belief systems, and we hope to honor this ideology by creating a business that focuses on caring for the planet, not just profits.",
-      code: "React",
+        "Inca Wheels is an indigenous owned outdoor recreational vehicle rental company based in the Pacific Northwest. Started as a way to help raise funds for the nonprofit Ruta Verde that focuses on Sustainable Development projects including the building of Community Forests in North America and creating sustainable food solutions in Peru. The Inca were honorable people that valued their surrounding environments at the core of their belief systems, and we hope to honor this ideology by creating a business that focuses on caring for the planet, not just profits.",
+      code: ["React"],
       deploy: "https://www.incawheels.com/",
       repo: "https://github.com/ColinJhEdwards/incawheels",
       img: [incaHome, incaVan, incaDest],
@@ -77,7 +79,7 @@ export const ProjectData = () => {
         "Murphy's Music School's mission is to bring their community together through the joys of music by providing a quality music education to students of all ages and skill levels.",
       longDesc:
         "Murphy's Music School's mission is to bring their community together through the joys of music by providing a quality music education to students of all ages and skill levels. The school has paved a new pathway in the industry of music education by offering lessons from experienced musicians and incorporating a performance-based education approach. Meaning the curriculum focuses on learning music concepts and applying them in a live performance.",
-      code: "React",
+      code: ["React"],
       deploy: "https://murphys-music-school.herokuapp.com/",
       repo: "https://github.com/ColinJhEdwards/murphy-music-school",
       img: [home, home1, home2, courses, teachers, contact1, contact2],
@@ -96,7 +98,7 @@ export const ProjectData = () => {
         "Sweep is an online database of information related to movies. Users are able to browse a collection of movies including popular, top rated, and movies currently playing in theaters.",
       longDesc:
         "Sweep is an online database of information related to movies. Users are able to browse a collection of movies including popular, top rated, and movies currently playing in theaters. Users are also able to search for specific movies and view a variety of information such as the movies trailer, release date, description, actors, and more.",
-      code: "React",
+      code: ["React"],
       deploy: "https://sweep-movies.herokuapp.com/",
       repo: "https://github.com/ColinJhEdwards/Sweep",
       img: [
@@ -116,7 +118,7 @@ export const ProjectData = () => {
         "Socks is a web-based application that allows users to save and view their current stock units.",
       longDesc:
         "Socks is a web-based application that allows a user to sign into their account, log their current stock units and cryptocurrency shares. Socks is a great choice for new and experienced investors alike. With an auto-populating, personalized dashboard showing infographics depicting the current value of your personalized market standings, this app brings simple viewings to your ever-so complicated investment portfolio. With a combination of rise in diversity of the stock trading market and an overall leaning preference to keep track of financial data in an easy-to-use application on your smart phone, Socks has never come at a better time. At Socks, our promise to raise the bar on UI will give anyone the opportunity to understand where their investments land at just one glance. Create an account and start logging to find out for yourself!",
-      code: "React",
+      code: ["React"],
       deploy: "https://socks-stocks.herokuapp.com/",
       repo: "https://github.com/ashley-ag/Socks",
       img: [socksHome, socksMarket, socksStocks],
@@ -129,7 +131,7 @@ export const ProjectData = () => {
         "Lee's Reptile Emporium is your one stop online shop for new pets ranging from a one inch dart drog to a 14 foot python and everything in between.",
       longDesc:
         "Lee's Reptile Emporium is your one stop online shop for new pets ranging from a 14 foot python to a 1 inch dart frog and everything in between. This web-based app will allow the user to view their future scaly family member, add animals to their cart and learn more about all of the amazing ways to incorporate them into your life.",
-      code: "JavaScript",
+      code: ["JavaScript"],
       deploy: "https://lees-reptile-emporium.herokuapp.com/",
       repo: "https://github.com/mlcohan/LeesReptileEmporium",
       img: [leeHome, leeBrowse, leeCollection, leeReviews],
@@ -142,7 +144,7 @@ export const ProjectData = () => {
         "A simple todo list for creating a list of items you must complete with functioning local storage",
       longDesc:
         "A simple todo list for creating a list of items you must complete with functioning local storage",
-      code: "JavaScript",
+      code: ["JavaScript"],
       deploy: "https://colinjhedwards.github.io/todoforme/",
       repo: "https://github.com/ColinJhEdwards/todoforme",
       img: [todoHome],
@@ -150,16 +152,21 @@ export const ProjectData = () => {
     },
     {
       id: 7,
-      title: "Mini Projects",
+      title: "Photographer Portfolio",
       Description:
-        "I love working with HTML, CSS, and Javascript so I created a webpage for demonstrating cool tricks you can do with these languages.",
+        "A basic template for a professional photographers personal portfolio.",
       longDesc:
-        "I love working with HTML, CSS, and Javascript so I created a webpage for demonstrating cool tricks you can do with these languages.",
-      code: "JavaScript",
-      deploy: "https://colinjhedwards.github.io/front-end-demos/",
-      repo: "https://github.com/ColinJhEdwards/front-end-demos",
-      img: [mini3D, miniSVG, miniSlide, miniInput],
-      mobileImg: [],
+        "A basic template for a professional photographers personal portfolio.",
+      code: ["Javascript"],
+      deploy: "https://colinjhedwards.github.io/photographer-portfolio/",
+      repo: "https://github.com/ColinJhEdwards/photographer-portfolio",
+      img: [photoHome, photoAbout, photoGallery, photoContact],
+      mobileImg: [
+        photoMobHome,
+        photoMobAbout,
+        photoMobGallery,
+        photoMobContact,
+      ],
     },
     {
       id: 8,
@@ -168,7 +175,7 @@ export const ProjectData = () => {
         "Aperture provides free stock photos that users can search through and use for personal projects, allowing them to create stunning designs and projects.",
       longDesc:
         "Aperture provides free stock photos that users can search through and use for personal projects, allowing them to create stunning designs and projects. All photos are pulled from pexels api which supplies hundreds of thousands free stock photos. Vists Pexels website to contribue your photos!",
-      code: "JavaScript",
+      code: ["JavaScript"],
       deploy: "https://colinjhedwards.github.io/Aperture/",
       repo: "https://github.com/ColinJhEdwards/Aperture",
       img: [aperHome, aperSearch],
